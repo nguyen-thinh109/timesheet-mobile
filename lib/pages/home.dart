0554112c9005b049868173257d9c4309e7836f77
nguyen-thinh109 <thinh.nguyenduc1@vdigital.vn>
2024-11-24 16:14:35 +0700

    create ui

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,32 +11,47 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   void _incrementCounter() {
-    setState(() {
-      });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: Image.asset('assets/images/timesheet.png', height: 70),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
-          child: Container(
-            alignment: Alignment.topLeft,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Xin chào,', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-                Text('Thịnh Nguyễn', style: TextStyle(fontSize: 22, fontWeight: FontWeight.w900)),
-              ],
+      // appBar: AppBar(
+      //   backgroundColor: Colors.transparent,
+      //   title: Image.asset('assets/images/timesheet.png', height: 70),
+      // ),
+      body: Stack(children: [
+        Center(
+          child: Image.asset('assets/images/timesheet.png', height: 70),
+        ),
+        Image.asset(
+          'assets/images/city1.png',
+          fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+          alignment: Alignment.center,
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 36, horizontal: 16),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Xin chào,',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                  Text('Thịnh Nguyễn',
+                      style:
+                          TextStyle(fontSize: 22, fontWeight: FontWeight.w900)),
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      ]),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
